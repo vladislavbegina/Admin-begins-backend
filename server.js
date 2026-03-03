911e902da4ca5c0b911a2d22372d779d7c8d80e2
vlad <_email@gmail.com>
2026-03-03 20:37:55 +0200

    add login system

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let users = [];
+
 app.get("/", (req, res) => {
   res.send("Admin Begins Backend Работает");
 });
@@ -17,10 +19,28 @@ app.post("/register", (req, res) => {
     return res.status(400).json({ message: "Заполните все поля" });
   }
 
-  res.json({
-    message: "Регистрация успешна",
-    user: { name, email }
-  });
+  const existingUser = users.find(u => u.email === email);
+  if (existingUser) {
+    return res.status(400).json({ message: "Пользователь уже существует" });
+  }
+
+  users.push({ name, email, password });
+
+  res.json({ message: "Регистрация успешна" });
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find(
+    u => u.email === email && u.password === password
+  );
+
+  if (!user) {
+    return res.status(401).json({ message: "Неверные данные" });
+  }
+
+  res.json({ message: "Вход успешен", token: "123456789" });
 });
 
 const PORT = process.env.PORT || 10000;
